3f204497d6c0448b685b499a47bcf4f0ba81464b
Tludi <tludi00@gmail.com>
2015-06-22 20:01:20 -0700

    added categories to posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  resources :categories
   resources :posts
   resources :sessions
   resources :users
  
-  root :to => "users#new"
+  root :to => "posts#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
