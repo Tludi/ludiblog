9e356809bb90f5af2141f0d5f0342f88ebb99377
Tludi <tludi00@gmail.com>
2015-06-29 12:46:37 -0700

    updating scope of recent posts on home page by date created

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,9 @@ class HomeController < ApplicationController
   def index
     @categories = Category.all
     @posts = Post.all.limit(4)
-    @allPosts = Post.all
-    @evenPosts = Post.where("id % 2 = 0").limit(5)
-    @oddPosts = Post.where("id % 2 != 0").limit(5)
-    @latestPost = Post.latest
+    @allPosts = Post.order(:updated_at => :asc)
+    @evenPosts = @allPosts.where("id % 2 = 0").limit(5)
+    @oddPosts = @allPosts.where("id % 2 != 0").limit(5)
+    @latestPost = Post.last
   end
 end
