6c7e3b99eb364be62b5a7b75acea5a126ee13d54
Tludi <tludi00@gmail.com>
2015-06-30 19:02:20 -0700

    updated navbar with login and logout. Removed some options.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,5 +7,9 @@ class HomeController < ApplicationController
     @evenPosts = @allPosts.where("id % 2 = 0").limit(5)
     @oddPosts = @allPosts.where("id % 2 != 0").limit(5)
     @latestPost = Post.last
+
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    end
   end
 end
